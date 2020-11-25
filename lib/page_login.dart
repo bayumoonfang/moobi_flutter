7d90c087ac89bacf26b8400110e83d234e2c8fd2
bayu.ragil <bayumoonfang@gmail.com>
2020-11-25 17:00:49 +0700

    pertama commit

--- lib/page_login.dart
+++ lib/page_login.dart
@@ -3,6 +3,7 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 class Login extends StatefulWidget {
   @override
@@ -43,13 +44,68 @@ class _LoginState extends State<Login> {
                       children: [
                           Padding(padding: const EdgeInsets.only(top:30,left: 15),
                             child: Align(alignment: Alignment.centerLeft,
-                              child: Text("Kenalin diri kamu", style: TextStyle(fontFamily: "VarelaRound",fontSize: 18,fontWeight: FontWeight.bold),),
+                              child: Text("Kenalin diri kamu", style: TextStyle(fontFamily: "VarelaRound",fontSize: 20,fontWeight: FontWeight.bold),),
                           ),),
                         Padding(padding: const EdgeInsets.only(top:5,left: 15),
                           child: Align(alignment: Alignment.centerLeft,
                             child: Text("Mulai dari identitas singkat kamu ya..",
                               style: TextStyle(fontFamily: "VarelaRound",fontSize: 13)),
-                          ),)
+                          ),),
+                        Padding(padding: const EdgeInsets.only(left: 15,top: 10,right: 90),
+                          child: TextFormField(
+                            style: TextStyle(fontFamily: "VarelaRound",fontSize: 15),
+                            decoration: new InputDecoration(
+                              contentPadding: const EdgeInsets.only(top: 1,left: 10,bottom: 1),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: HexColor("#DDDDDD"), width: 1.0),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: HexColor("#DDDDDD"), width: 1.0),
+                              ),
+                              hintText: 'Nama Lengkap',
+                            ),
+                          ),
+                        ),
+                        Padding(padding: const EdgeInsets.only(left: 15,top: 10,right: 90),
+                          child: TextFormField(
+                            style: TextStyle(fontFamily: "VarelaRound",fontSize: 15),
+                            decoration: new InputDecoration(
+                              contentPadding: const EdgeInsets.only(top: 1,left: 10,bottom: 1),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: HexColor("#DDDDDD"), width: 1.0),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: HexColor("#DDDDDD"), width: 1.0),
+                              ),
+                              hintText: 'Email',
+                            ),
+                          ),
+                        ),
+                        Padding(padding: const EdgeInsets.only(left: 15,top: 10,right: 90),
+                          child: TextFormField(
+                            style: TextStyle(fontFamily: "VarelaRound",fontSize: 15),
+                            decoration: new InputDecoration(
+                              contentPadding: const EdgeInsets.only(top: 1,left: 10,bottom: 1),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: HexColor("#DDDDDD"), width: 1.0),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: HexColor("#DDDDDD"), width: 1.0),
+                              ),
+                              hintText: 'Password',
+                            ),
+                          ),
+                        ),
+
+                        Padding(padding: const EdgeInsets.only(top:50,left: 15),
+                          child: Align(alignment: Alignment.centerLeft,
+                            child: Text("Toko Kamu", style: TextStyle(fontFamily: "VarelaRound",fontSize: 20,fontWeight: FontWeight.bold),),
+                          ),),
+                        Padding(padding: const EdgeInsets.only(top:5,left: 15),
+                          child: Align(alignment: Alignment.centerLeft,
+                            child: Text("Yuk Kenalin toko kamu ya..",
+                                style: TextStyle(fontFamily: "VarelaRound",fontSize: 13)),
+                          ),),
                       ],
                     ),
               ),
