d2b3f471bb67f470bd4a2b2ef95411f06086dc09
bayu.ragil <bayumoonfang@gmail.com>
2021-04-15 16:39:32 +0700

    15042021

--- lib/Profile/page_subscibe.dart
+++ lib/Profile/page_subscibe.dart
@@ -14,6 +14,8 @@ import 'package:moobi_flutter/Helper/page_route.dart';
 import 'package:moobi_flutter/Helper/session.dart';
 import 'package:moobi_flutter/Profile/page_profile.dart';
 import 'package:moobi_flutter/page_login.dart';
+import 'package:responsive_container/responsive_container.dart';
+import 'package:steps/steps.dart';
 import 'package:toast/toast.dart';
 import 'package:http/http.dart' as http;
 import 'package:moobi_flutter/Helper/api_link.dart';
@@ -157,9 +159,123 @@ class SubscribeState extends State<Subscribe> {
                       children: [
                         Align(alignment: Alignment.centerLeft,child: Padding(
                           padding: const EdgeInsets.only(left: 0,top: 15),
-                          child: Text("Nama Anda",style: TextStyle(fontWeight: FontWeight.bold,fontFamily: "VarelaRound",
-                              fontSize: 12,color: HexColor("#0074D9")),),
+                          child:
+                          Text("Segera subscribe Moobie anda",
+                            style: TextStyle(fontWeight: FontWeight.bold,fontFamily: "VarelaRound",
+                              fontSize: 16,color: Colors.black),),
                         ),),
+                        Align(alignment: Alignment.centerLeft,child: Padding(
+                          padding: const EdgeInsets.only(left: 0,top: 10, right: 35),
+                          child:
+                          Text("Sudah nyaman dengan moobie ? yuk terus kembangkan bisnis kamu , dengan moobie. "
+                              "Segera dapatkan full version moobie dengan "
+                              "cara subscribe dan dapatkan keuntungan lainnya serta update "
+                              "terbaru yang keren habis dari moobie.",
+                            style: TextStyle(fontFamily: "VarelaRound",
+                                fontSize: 12,color: Colors.black,height: 1.5),),
+                        ),),
+
+                        Align(alignment: Alignment.centerLeft,child: Padding(
+                          padding: const EdgeInsets.only(left: 0,top: 30, right: 35),
+                          child:
+                          Text("Mulai dari",
+                            style: TextStyle(fontFamily: "VarelaRound",fontWeight: FontWeight.bold,
+                                fontSize: 12,color: Colors.black,height: 1.5),),
+                        ),),
+                        Align(alignment: Alignment.centerLeft,child: Padding(
+                          padding: const EdgeInsets.only(left: 0,top: 5, right: 35),
+                          child:
+                               Row(
+                                 children: [
+                                   Text("Rp.20.000",
+                                       style: TextStyle(fontFamily: "VarelaRound",fontWeight: FontWeight.bold,
+                                           fontSize: 28,color: HexColor("#2ECC40"))),
+                                   Padding(padding: const EdgeInsets.only(top: 7,left: 5),
+                                   child: Text("/ bulan",
+                                       style: TextStyle(fontFamily: "VarelaRound",
+                                           fontSize: 14,color: HexColor("#AAAAAA"))),)
+                                 ],
+                               )
+                        ),),
+                        Align(alignment: Alignment.centerLeft,child: Padding(
+                          padding: const EdgeInsets.only(left: 0,top: 5, right: 35),
+                          child:
+                     Opacity(
+                       opacity: 0.6,
+                       child: Text("*Sudah termasuk PPN , dan biaya admin",
+                         style: TextStyle(fontFamily: "VarelaRound",
+                             fontSize: 10,color: Colors.black),),
+                     )
+                        ),),
+
+
+                        Padding(padding: const EdgeInsets.only(top :20),
+                          child: Container(
+                            height: 7,
+                            width: double.infinity,
+                            color: HexColor("#f4f4f4"),
+                          ),),
+
+                        Align(alignment: Alignment.centerLeft,child: Padding(
+                          padding: const EdgeInsets.only(left: 0,top: 10, right: 35),
+                          child:
+                          ResponsiveContainer(
+                            widthPercent: 100,
+                            heightPercent: 85,
+                                child:        Steps(
+                                  direction: Axis.vertical,
+                                  size: 10.0,
+                                  path: {
+                                    'color': HexColor("#DDDDDD"),
+                                    'width': 1.0},
+                                  steps: [
+                                    {
+                                      'color': Colors.white,
+                                      'background': HexColor(main_color),
+                                      'label': '1',
+                                      'content': Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: <Widget>[
+                                          Text(
+                                            'Pembayaran',
+                                            style: TextStyle(fontSize: 22.0),
+                                          ),
+                                          Text(
+                                            'Lakukan pembayaran untuk memulai subscribe aplikasi moobie. Anda dapat melakukan pembayaran ke beberapa rekening atau layanan ',
+                                            style: TextStyle(fontSize: 12.0),
+                                          ),
+                                        ],
+                                      ),
+                                    },
+
+                                    {
+                                      'color': Colors.white,
+                                      'background': Colors.lightBlue.shade200,
+                                      'label': '1',
+                                      'content': Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: <Widget>[
+                                          Text(
+                                            'Laborum exercitation',
+                                            style: TextStyle(fontSize: 22.0),
+                                          ),
+                                          Text(
+                                            'Qui et consectetur esse duis excepteur magna consectetur.',
+                                            style: TextStyle(fontSize: 12.0),
+                                          ),
+                                        ],
+                                      ),
+                                    },
+
+                                  ],
+
+                                ),
+                              )
+                        ),),
+
+
+
+
 
                       ],
                     )
